9d0455d8efa6fb7986529fb53c13fa310a3f397b
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2017-01-13 17:07:36 +0000

    Clean test data

--- tests/services/obd2_data_service_test.go
+++ tests/services/obd2_data_service_test.go
@@ -4,11 +4,15 @@ package services_test
 import (
 	"github.com/steventux/obd2-data-api/models"
 	"github.com/steventux/obd2-data-api/services"
+	"github.com/tborisova/clean_like_gopher"
 	"gopkg.in/mgo.v2/bson"
 	"testing"
 )
 
 func TestSaveObd2Data(t *testing.T) {
+	options := map[string]string{"host": "localhost", "dbName": "test", "port": "27017"}
+	m, _ := clean_like_gopher.NewCleaningGopher("mongo", options)
+
 	services.SaveObd2Data(&models.Obd2Data{
 		Torque:    "198",
 		Voltage:   "14.4",
@@ -20,6 +24,9 @@ func TestSaveObd2Data(t *testing.T) {
 	}
 
 	err := services.Obd2DataCollection().Find(bson.M{"voltage": "14.4"}).One(&result)
+
+	m.Clean(nil)
+	m.Close()
 
 	if err != nil {
 		t.Errorf("error while retrieving data: %v", err)
